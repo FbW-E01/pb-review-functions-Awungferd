1d4abebfe129b40d0ec8b7f1e42839069c705ece
Ferdinand Mbecha <75342805+Awungferd@users.noreply.github.com>
2021-05-30 14:44:22 +0200

    almost done!

--- functionsReview.js
+++ functionsReview.js
@@ -31,7 +31,13 @@ const people = (arr) => {
     return arr.shift(1)
 }
 
-const arrowArr = ["Peter", "Mary", "Lando", "Aurlus", "Li"]
+const arrowArr = [
+    "Peter",
+    "Mary",
+    "Lando",
+    "Aurlus",
+    "Li"
+]
 console.log(people(arrowArr))
 
 // 5. Write an arrow function that takes in a string and * returns * the last three characters of that string.
@@ -43,20 +49,43 @@ const word = "gainsaid"
 console.log(slicedLastThree(word))
 
 // 6. Write an arrow function that takes in an array and * returns * the type of the first item in that array.
-let someArray =  ["spinning", 200, 669, 235, "choleric","tantrum","cynicism"];
-stringType= ()=> { 
-    console.log(typeof someArray[0] )
+let someArray = [
+    "spinning",
+    200,
+    669,
+    235,
+    "choleric",
+    "tantrum",
+    "cynicism"
+];
+stringType = () => {
+    console.log(typeof someArray[0])
 }
 stringType()
 // 7. Write an arrow function that takes in an array and * returns * true, if all items in that array have the same type.
-let someArray =  ["Cao",true, true, true];
-let result=arr=>arr.every(e=>e===true)
-if (result(someArray)===true) { console.log(true)
-    
-} else {console.log("Swipe back once more")
+let someArray1 = ["Cao", true, true, true];
+let result = arr => arr.every(e => e === true)
+if (result(someArray1) === true) {
+    console.log(true)
+
+} else {
+    console.log("Sorry condition not met!")
 }
 // 8. Print the`type` of a variable that has a normal function value.
+function someElse(e,f) {
+    return e%f
+    }
+let another= someElse(40,3)// The type of the value for variable "another" is a normal function
+console.log(another)
+console.log(typeof someElse)
+
 // 9. Print the`type` of a variable that has an arrow function value.
+let someElse1=(g,h)=> {
+    return (g/h).toFixed(0)
+    }
+let another2= someElse1(40,3)
+console.log(another2)
+console.log(typeof someElse1)
 // 10. Write and test a function that takes in two parameters(min and max).That function should return another function that doesn't take in any parameters, but returns a random number between min and max. Effectively your "outer" function is a factory that creates customized random number generator functions.
 // 11. In your own words, explain what are side effects and what is a pure function.
 // 12. In your own words, explain what is the difference between functions and methods.
--- testfunctions.js
+++ testfunctions.js
@@ -1,2 +1,6 @@
-
-
+let someElse1=(g,h)=> {
+    return (g/h).toFixed(0)
+    }
+let another2= someElse1(40,3)
+console.log(another2)
+console.log(typeof someElse1)
